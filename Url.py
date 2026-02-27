4c82dfc73747389a23dca3a6a784bf0675a8ffc5
Nandini122004 <ns242315@gmail.com>
2026-02-27 20:55:12 +0530

    Delete Url.py

--- Url.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from authentication.views import RegisterView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/register/', RegisterView.as_view(), name='register'),
-]
